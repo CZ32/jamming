53ca179c05f02d8e9e382e929aab099bf38af192
Cengizhan Ziyaeddin <cengiz.ziyaeddin@sigmalabs.xyz>
2021-08-19 12:10:31 +0100

    Search term state added to component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -24,8 +24,10 @@ class App extends React.Component {
   this.removeTrack = this.removeTrack.bind(this);
   this.updatePlaylistName = this.updatePlaylistName.bind(this);
   this.savePlaylist = this.savePlaylist.bind(this);
+  this.search = this.search.bind(this);
  }
 
+ //Adds track to playlist
  addTrack(track){
    if(this.state.playlistTracks.find
     (savedTrack=>savedTrack.id === track.id)){
@@ -36,6 +38,7 @@ class App extends React.Component {
     }
  }
 
+ //Removes track from playlist
  removeTrack(track){
    let tracks = this.state.playlistTracks;
    if(tracks.filter(removeTrack => removeTrack.id === track.id)){
@@ -46,12 +49,19 @@ class App extends React.Component {
    }
  }
 
+ //Updates playlist name.
  updatePlaylistName(name){
    this.setState({...this.state, playlistName: name})
  }
 
+ //Saves track url to an array 
  savePlaylist(){
    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+ }
+
+ //Searched spotify by term provided in search bar. 
+ search(term){
+   console.log(term)
  }
 
  render() {
@@ -59,7 +69,7 @@ class App extends React.Component {
    <div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-    <SearchBar/>
+    <SearchBar onSearch={this.search}/>
     <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
       <Playlist playlistName={this.state.playlistName} 
--- src/components/search-bar/searchBar.js
+++ src/components/search-bar/searchBar.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './searchBar.css'
 
 class SearchBar extends React.Component {
+constructor(props){
+    super(props)
+    this.search = this.search.bind(this)
+    this.handleTermChane = this.handleTermChane.bind(this)
+}
+search(){
+    this.props.onSearch(this.state.searchTerm)
+
+}
+
+handleTermChane(e){
+    this.setState({ searchTerm: e.target.value })
+}
+
  render() {
   return (
    <div className="SearchBar">
-    <input placeholder="Enter a song, album, or artist" />
+    <input placeholder="Enter a song, album, or artist" onChange={(e) => this.handleTermChane(e)} />
     <button className="SearchButton">SEARCH</button>
    </div>
   );
